2757912e5ac790196fb1f58253b037b4bcded751
aaloksikand <aalok.sikand@gmail.com>
2022-05-31 22:15:16 -0700

    Draft with comments, readme file and revisions curings bugs related to nAn entries

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Password Generator Starter Code
+# Password Generator
+## Description
+We have made the following additions to the password generator:
+1.  We created a function called generatePassword;
+2.  We asked the user what kind of characters they would like in their password (small caps, large caps, numerics, special characters);
+3.  We asked the user the password length between 8 and 128 characters;
+4.  We created if/then/while statements accounting for all possible scenarios;
+5.  We used the "concat" method to combine strings together;
+6.  We randomized the password string and returned a string value of the user's choice using the ".charAt," "Math.floor," "Math.random," and ".length" functions;
+## Screenshot
+![Image Caption](assets/Aaloks%20Amazing%20Password%20Generator.png)
+## Link
+https://aaloksikand.github.io/Password_Generator/
--- script.js
+++ script.js
@@ -1,147 +1,129 @@
 
+var generateBtn = document.querySelector("#generate");  //This will write the password in the generatebtn box
 
-// //Technical Acceptance Criteria: 40%
-// Application deployed at live URL.
-// Application loads with no errors.
-// Application GitHub URL submitted.
-// GitHub repository that contains application code.
-// Application user experience is intuitive and easy to navigate.
-// Application user interface style is clean and polished.
-// Application resembles the mock-up functionality provided in the Challenge instructions.
-// Repository has a unique name.
-// Repository follows best practices for file structure and naming conventions.
-// Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-// Repository contains multiple descriptive commit messages.
-// Repository contains a quality README file with description, screenshot, and link to deployed application
-// You are required to submit BOTH of the following for review:
-// The URL of the deployed application.
-// The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.//
-
-// // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+function writePassword() 
+{
+  var password = generatePassword();  //variable 'password' will equal what generatePassword function returns
+  var passwordText = document.querySelector("#password");  //id #password in html document will equal passwordText
+  passwordText.value = password; //the value of passwordText equals password
 }
 
-// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  window.alert("Welcome to Aalok's Amazing Password Generator!");
-  // We will generate a password based on the following preferences:
-  // (1) preferred length between 8 to 128 characters;
-  // (2) at least one lowercase letter;
-  // (3) at least one uppercase letter;
-  // (4) at least one numeric character;
-  // (5) at least one of the following special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)")
+function generatePassword() 
+{
+  window.alert("Welcome to Aalok's Amazing Password Generator!"); //This is my clever alert
 
-  var smallCapsInput = window.confirm(
-    "Press 'OK' if you would like lowercase letters in your random password."
-  );
-  var largeCapsInput = window.confirm(
-    "Press 'OK' if you would like uppercase letters in your password."
-  );
-  var numericsInput = window.confirm(
-    "Press 'OK' if you would like numerics in your password."
-  );
-  var yourSpecialInput = window.confirm(
-    "Press 'OK' if you would like special characters in your random password."
-  );
-  var yourPasswordLength 
-  var unvalidInput = true;
-  while(unvalidInput)
+  var yourPasswordLength //Creating a variable to store the password length from 8 to 128 characters that the user specifies
+  var invalidInput = true;  //Creeating a feedback loop if user enters something other than a numerical value between 8 and 128
+  while(invalidInput)  //creating a while loop
   {
     yourPasswordLength = window.prompt(
-        "How many characters would your like your password?  Please a value from 8 to 128 characters only."
+        "Please enter your desired length of password between 8 and 128 characters only."
     );
-    unvalidInput = yourPasswordLength < 8 || yourPasswordLength > 128
+    invalidInput = yourPasswordLength < 8 || yourPasswordLength > 128 || isNaN(yourPasswordLength)  //This will keep asking for a valid number if user selects a non-number or a number outside of the 8-128 range
   }
-  
 
+  var smallCapsInput = window.confirm  //stores whether user wants small caps
+  (
+    "Would you like lowercase letters in your random password? Press 'OK' for 'Yes' or 'Cancel' for 'No'."  //asks users whether they want small caps
+  );
+  var largeCapsInput = window.confirm  //stores whether user wants large caps
+  (
+    "Would you like uppercase letters in your random password? Press 'OK' for 'Yes' or 'Cancel' for 'No'."  //asks users whether they want large caps
+  );
+  var numericsInput = window.confirm  //stores whether user wants numerics
+  (
+    "Would you like numerics in your random password? Press 'OK' for 'Yes' or 'Cancel' for 'No'."  //asks users whether they wants numerics
+  );
+  var yourSpecialInput = window.confirm  //stores users whether they want special characters
+  (
+    "Would you like special characters in your random password? Press 'OK' for 'Yes' or 'Cancel' for 'No'."  //asks users whether they wants special characters
+  );
 
   var smallCaps = "abcdefghijklmnopqrstuvwxyz"; //create variable for small caps string
   var largeCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //create variable for large caps string
   var numerics = "0123456789"; //create variable for numerics string
-  var yourSpecial = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var yourSpecial = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" //create a variable for special characters string
 
-  console.log(smallCapsInput);
-  console.log(largeCapsInput);
-  console.log(numericsInput);
-  console.log(yourSpecialInput);
-  console.log(yourPasswordLength);
-  var passwordOptions = "" //empty string
- 
+  console.log(smallCapsInput);  //tests stored value
+  console.log(largeCapsInput);  //tests stored value
+  console.log(numericsInput);  //tests stored value 
+  console.log(yourSpecialInput);  //tests stored value
+  console.log(yourPasswordLength);  //tests stored value
+  var passwordOptions = "" //empty string for now
+ // over a dozen variations the user can submit
   if (smallCapsInput == true && largeCapsInput == true && numericsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = smallCaps.concat(largeCaps, numerics, yourSpecial)
+      passwordOptions = smallCaps.concat(largeCaps, numerics, yourSpecial)  //concats the desired strings together 
   }
   else if (smallCapsInput == true && largeCapsInput == true && numericsInput == true)
   {
-      passwordOptions = smallCaps.concat(largeCaps, numerics)
+      passwordOptions = smallCaps.concat(largeCaps, numerics) //concats the desired strings together 
   }
   else if (smallCapsInput == true && largeCapsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = smallCaps.concat(largeCaps, yourSpecial)
+      passwordOptions = smallCaps.concat(largeCaps, yourSpecial)  //concats the desired strings together 
   }
   else if (smallCapsInput == true && numericsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = smallCaps.concat(numerics, yourSpecial)
+      passwordOptions = smallCaps.concat(numerics, yourSpecial)  //concats the desired strings together 
   }
   else if (largeCapsInput == true && numericsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = largeCaps.concat(numerics, yourSpecial)
+      passwordOptions = largeCaps.concat(numerics, yourSpecial)  //concats the desired strings together 
   }
   else if (smallCapsInput == true && largeCapsInput == true)
   {
-      passwordOptions = smallCaps.concat(largeCaps)
+      passwordOptions = smallCaps.concat(largeCaps)  //concats the desired strings together 
   }
   else if (smallCapsInput == true && numericsInput == true)
   {
-      passwordOptions = smallCaps.concat(numerics)
+      passwordOptions = smallCaps.concat(numerics)  //concats the desired strings together 
   }
   else if (smallCapsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = smallCaps.concat(yourSpecial)
+      passwordOptions = smallCaps.concat(yourSpecial)  //concats the desired strings together 
   }
   else if (largeCapsInput == true && numericsInput == true)
   {
-      passwordOptions = largeCaps.concat(numerics)
+      passwordOptions = largeCaps.concat(numerics)  //concats the desired strings together 
   }
   else if (largeCapsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = largeCaps.concat(yourSpecial)
+      passwordOptions = largeCaps.concat(yourSpecial)  //concats the desired strings together 
   }
   else if (numericsInput == true && yourSpecialInput == true)
   {
-      passwordOptions = numerics.concat(yourSpecial)
+      passwordOptions = numerics.concat(yourSpecial)  //concats the desired strings together 
   }
   else if (smallCapsInput == true)
   {
-    passwordOptions = smallCaps;
+    passwordOptions = smallCaps;  //concats the desired strings together 
   }
   else if (largeCapsInput == true)
   {
-      passwordOptions = largeCaps;
+      passwordOptions = largeCaps;  //concats the desired strings together 
   }
   else if (numericsInput == true)
   {
-     passwordOptions = numerics;
+     passwordOptions = numerics;  //concats the desired strings together 
   }
   else if (yourSpecialInput == true)
   {
-     passwordOptions = yourSpecial;
+     passwordOptions = yourSpecial;  //concats the desired strings together 
+  }  
+  else if (smallCapsInput == false && largeCapsInput == false && numericsInput == false && yourSpecialInput == false)
+  {
+    alert("You must pick at least one type of character for your auto-generated password.")  //returns an error message if no types of characters have been selected
+    generatePassword();  //returns an error message if no types of characters have been selected
   }
-
-  console.log(passwordOptions)
-  var result = "";
+  console.log(passwordOptions)  //tests the password string
+  var result = "";  //creates an empty string called 'result'
   for ( var i = 0; i < yourPasswordLength; i++ ) {
     result += passwordOptions.charAt(Math.floor(Math.random() * 
-passwordOptions.length));
+passwordOptions.length));  //creates randomized number of all desired characters as long as user wants
  } 
- return result;
+ return result;  //returns value into variable 'password' created by generatePassword function
 }
 
